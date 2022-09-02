6e20bd44269cf65d7b98ae95335dc09b30b246fb
olgasupr <101745919+olgasupr@users.noreply.github.com>
2022-09-02 10:22:45 +0300

    Initial project setup

---
+++ src/main/java/com/olga/familyfinancemanagement/FamilyFinanceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.olga.familyfinancemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFinanceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyFinanceManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olga/familyfinancemanagement/FamilyFinanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olga.familyfinancemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFinanceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
