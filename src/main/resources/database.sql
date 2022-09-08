fe91199925b0ab84b95dfe0253a85f29d190b8a5
Olga L <olga.lesovska@gmail.com>
2022-09-08 11:25:18 +0300

    Initial database SQL script

---
+++ src/main/resources/database.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE IF NOT EXISTS Family_Finance_Management;
+
+USE Family_Finance_Management;
+
+CREATE TABLE IF NOT EXISTS period(
+    id int NOT NULL auto_increment,
+    from_date DATE,
+	to_date DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS income(
+    id int NOT NULL auto_increment,
+    amount DECIMAL(10, 2),
+    FOREIGN KEY(id) REFERENCES period (id),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS category(
+    id int NOT NULL auto_increment,
+    category_name VARCHAR(50) NOT NULL UNIQUE,
+	description VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS target_spending(
+	id int NOT NULL auto_increment,
+    period_id INT NOT NULL,
+	category_id INT NOT NULL,
+    target_percent DECIMAL(3,2),
+	within_income BOOLEAN,
+    FOREIGN KEY(period_id) REFERENCES period (id),
+    FOREIGN KEY (category_id) REFERENCES category (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS actual_spending(
+	id int NOT NULL auto_increment,
+    category_id INT NOT NULL,
+	amount DECIMAL(10, 2),
+	date TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES category (id),
+    PRIMARY KEY (id)
+);
+
+select * from period;
+select * from income;
+select * from category;
+select * from target_spending;
+select * from actual_spending;
