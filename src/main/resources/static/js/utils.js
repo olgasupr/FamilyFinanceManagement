aa41783b424186213cf67cf371773fbe3922699a
olgasupr <101745919+olgasupr@users.noreply.github.com>
2022-09-14 22:34:58 +0300

    Fix weird edge cases with input formats

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,14 +1,10 @@
 
-//Formats a box while it is being typed into.  Examples:
-// 123 --> 123
-// 1234 --> 1,234
-// 1234.567 --> 1,234.56 (only two decimals are preserved)
-// 1234.  -->  1,234.  (preserve the ., as need it to type.)
 function formatInputAmount(amount) {
     var parts = amount.split('.');
+    let whole = (parseInt(amount) || 0).toLocaleString('en-US');
     if (parts.length == 1) {
-        return parseInt(amount).toLocaleString('en-US');
+        return whole;
     } else {
-        return parseInt(amount).toLocaleString('en-US') + '.' + parts[1].substring(0,2);
+        return whole + '.' + parts[1].substring(0,2);
     }
 }
