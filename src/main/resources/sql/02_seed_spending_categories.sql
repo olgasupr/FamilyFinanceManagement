e85ead6d0817da707de1ecc168c20281ed77c241
olgasupr <101745919+olgasupr@users.noreply.github.com>
2022-09-14 21:52:42 +0300

    Create 02_seed_spending_categories.sql
    Seed spending categories

---
+++ src/main/resources/sql/02_seed_spending_categories.sql
@@ -0,0 +1,12 @@
+USE family_finance_management;
+INSERT INTO spending_category (category_name, description)
+VALUES
+('Food', 'Groceries, restaurants, cafes, etc.'),
+('Personal', 'Clothing, hobbies, self-care, home decor, etc.'),
+('Transportation', 'Public transport, car payments, fuel, maintenance, etc.'),
+('Housing', 'Mortgage, rent, fees, taxes, etc.'),
+('Utilities', 'Gas, electric, water, internet, cell phone, etc.'),
+('Savings', 'Retirement savings, pension funds, emergency fund, etc.'),
+('Insurance', 'Health, house, auto, life, etc.'),
+('Recreation', 'Travel, entertainment, lifestyle expenses, etc.'),
+('Giving', 'Donation, etc.')
