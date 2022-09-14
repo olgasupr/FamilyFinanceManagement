cb844580e852a4ce232d39ae55c033c9f253e559
olgasupr <101745919+olgasupr@users.noreply.github.com>
2022-09-14 23:28:09 +0300

    work on month control, not finished

--- src/main/java/com/olga/familyfinancemanagement/controllers/MainController.java
+++ src/main/java/com/olga/familyfinancemanagement/controllers/MainController.java
@@ -3,11 +3,18 @@ package com.olga.familyfinancemanagement.controllers;
 import com.olga.familyfinancemanagement.models.Income;
 import com.olga.familyfinancemanagement.services.IncomeService;
 import com.olga.familyfinancemanagement.services.SpendingService;
+import com.olga.familyfinancemanagement.utils.DateUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+import static java.time.ZoneOffset.UTC;
+import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
 
 @AllArgsConstructor
 @Controller
@@ -53,4 +60,20 @@ public class MainController {
         return "analysis";
     }
 
+    @GetMapping(value = "/month")
+    public String dummyMonthSelector(
+            @RequestParam(value = "yyyymm", required = false) String yyyymm,
+            Model model) {
+
+        Date parsedMonth = DateUtils.parseYYYYMM(yyyymm);
+        Date thisMonth = parsedMonth == null ?
+                DateUtils.getFirstDayOfCurrentMonth() :
+                parsedMonth;
+
+        model.addAttribute("thisMonth", thisMonth);
+        model.addAttribute("lastMonth", DateUtils.addMonths(thisMonth, -1));
+        model.addAttribute("nextMonth", DateUtils.addMonths(thisMonth, 1));
+
+        return "month";
+    }
 }
--- src/main/java/com/olga/familyfinancemanagement/utils/DateUtils.java
+++ src/main/java/com/olga/familyfinancemanagement/utils/DateUtils.java
@@ -1,5 +1,7 @@
 package com.olga.familyfinancemanagement.utils;
 
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 
 import static java.time.LocalDate.now;
@@ -15,5 +17,29 @@ public class DateUtils {
 
     public static Date getLastDayOfCurrentMonth() {
         return Date.from(now().with(lastDayOfMonth()).atStartOfDay().toInstant(UTC));
+    }
+
+    public static Date addMonths(Date date, int months) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.MONTH, months);
+        return c.getTime();
+    }
+
+    public static Date parseYYYYMM(String yyyymm) {
+        try {
+            int yyyymmAsInt = Integer.parseInt(yyyymm);
+            int year = yyyymmAsInt / 100;
+            int month = yyyymmAsInt % 100;
+
+            if (year > 2000 && year < 2100 && month >= 1 && month <= 12) {
+                return Date.from(LocalDate.of(year, month, 1).atStartOfDay().toInstant(UTC));
+            }
+            // invalid date
+            return null;
+        } catch (NumberFormatException e) {
+            //failed to parse
+            return null;
+        }
     }
 }
