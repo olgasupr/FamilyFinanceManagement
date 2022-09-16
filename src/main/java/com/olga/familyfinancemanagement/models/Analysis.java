9bdcb181882d93c9e8a82ac35cb6114515b2679d
olgasupr <101745919+olgasupr@users.noreply.github.com>
2022-09-16 10:36:21 +0300

    Implement Analysis.cs

--- src/main/java/com/olga/familyfinancemanagement/models/Analysis.java
+++ src/main/java/com/olga/familyfinancemanagement/models/Analysis.java
@@ -13,4 +13,36 @@ public class Analysis {
     private Income income;
     private TargetSpending targetSpending;
     private double actualSpendingAmount;
+
+    public String getSpendingCategoryName() {
+        return spendingCategory.getCategoryName();
+    }
+
+    public String getSpendingCategoryDescription() {
+        return spendingCategory.getDescription();
+    }
+
+    public double getIncomeAmount() {
+        return income.getAmount();
+    }
+
+    public double getTargetSpendingPercent() {
+        return targetSpending.getTargetPercent();
+    }
+
+    public double getTargetSpendingAmount() {
+        return getTargetSpendingPercent() * getIncomeAmount();
+    }
+
+    public double getActualSpendingPercent() {
+        return getActualSpendingAmount() / getIncomeAmount();
+    }
+
+    public double getExcessPercent() {
+        return getActualSpendingPercent() - getTargetSpendingPercent();
+    }
+
+    public double getExcessAmount() {
+        return getActualSpendingAmount() - getTargetSpendingAmount();
+    }
 }
