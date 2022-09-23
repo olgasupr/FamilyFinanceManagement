7a9e1d06a47eb454b8b4f30a53992d9513330058
olgasupr <101745919+olgasupr@users.noreply.github.com>
2022-09-23 12:02:01 +0300

    Update target_spending

--- src/main/resources/sql/05_seed_target_percentages.sql
+++ src/main/resources/sql/05_seed_target_percentages.sql
@@ -7,7 +7,7 @@ VALUES
 ('66', '0.15'), -- Transportation
 ('67', '0.3'),  -- Housing
 ('68', '0.1'),  -- Utilities
-('69', '0.1'),  -- Savings
+('69', '0.15'),  -- Savings
 ('70', '0.2'),  -- Insurance
 ('71', '0.1'),  -- Recreation
 ('72', '0.05')  -- Giving
